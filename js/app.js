f736b9ce7458c1b1e47006275d4b367958326955
SudhinRaj <58643406+rjsudhin@users.noreply.github.com>
2024-08-17 20:59:20 +0530

    getting user input will working and taking input from the user

--- js/app.js
+++ js/app.js
@@ -1,7 +1,28 @@
 let color = '#252525'
 
+const selectionSizePopBtn = document.querySelector('.size-selection-btn')
+
+selectionSizePopBtn.addEventListener('click', (e) => {
+    const userInput = gettingUserInputSize()
+    loadingContainerBoards(userInput)
+})
+
 // making 16 x 16 display when load every time
 loadingContainerBoards(16)
+
+// getting user input size
+function gettingUserInputSize() {
+    let inputResult = false
+    while (inputResult == false) {
+        let input = prompt('Enter the size')
+        if (input == null || input == '') {
+            inputResult = false
+        } else {
+            inputResult = true
+            return input
+        }
+    }
+}
 
 // creation of boards of container
 function loadingContainerBoards(size) {
