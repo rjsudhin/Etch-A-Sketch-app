4d68a0d25fc108381954f3bad80fcf7099f07bcd
SudhinRj <rjsudhin@users.noreply.github.com>
2025-09-12 11:00:28 +0530

    added the size popup button to entering user input size of the grid

--- drawing-app.js
+++ drawing-app.js
@@ -1,3 +1,4 @@
+const sizePopupBtn = document.querySelector('.size-popup')
 const drawingGrid = document.querySelector('#drawingGrid')
 
 
@@ -16,6 +17,32 @@ function loadingGridContainer(size) {
   }
 }
 
+// size popup button clicks
+sizePopupBtn.addEventListener('click', () => {
+  let userInputsize = gettingNewGrid()
+  loadingGridContainer(userInputsize)
+
+})
+
+// getting new size of grid with prompt result
+function gettingNewGrid() {
+  let inputResult = false
+  while (!inputResult) {
+    let userInput = parseInt(prompt('Give a size to new grid'))
+    if (!userInput == '' || !userInput == null) {
+      if (userInput < 100 && userInput > 3) {
+        console.log('user entered value ' + userInput)
+        inputResult = true
+        return userInput
+      } else {
+        console.log('please the value in the limit')
+      }
+    }
+    else {
+      console.log('Enter a valid size')
+    }
+  }
+}
 
 // drawing each box when the event occurs
 function drawingBoxes(e) {
